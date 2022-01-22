592375514b6ef238c3e678935890f403a9b46cf9
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-01-22 20:37:09 +0300

    ViewModel

---
+++ MVVM+DataDrivenUI/MainModule/Model/ViewData.swift
@@ -0,0 +1,23 @@
+//
+//  ViewData.swift
+//  MVVM+DataDrivenUI
+//
+//  Created by MacBookPro on 22.01.2022.
+//
+
+import Foundation
+
+enum ViewData {
+    case initial
+    case loading(Data)
+    case success(Data)
+    case failure(Data)
+    
+    struct Data {
+        let icon: String?
+        let title: String?
+        let description: String?
+        let numberPhone: String?
+    }
+    
+}
